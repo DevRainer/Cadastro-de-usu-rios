63f0bfe18a5f15850da95bf6ad6d9babd8089dd9
DevRainer <rainergsilva@hotmail.com>
2025-11-12 13:05:44 -0300

    fix(ListUser): carregar array de usuários ao iniciar a página

--- src/pages/ListUsers/index.jsx
+++ src/pages/ListUsers/index.jsx
@@ -1,30 +1,50 @@
 
-import { useEffect } from "react"
+import { useEffect, useState} from "react"
+import { useNavigate } from "react-router-dom"
 import api from "../../services/api.js"
-
 import { Button } from "../../components/Button/styles"
 import TopBackground from "../../components/TopBackground/index.jsx"
+import {Title} from './styles'
+import {Container, ContainerUser, CardUsers, TrashIcon,} from './styles'
+import Trash from "../../assets/trash.svg"
+
 
 
 function ListUsers() {
+const [users, setUsers] = useState([]);
+   const navigate = useNavigate(); 
 
 useEffect(() => {
  
     async function getUsers() {
-        const usersFromApi = await api.get('/usuarios');
-        console.log(usersFromApi)
+        const {data} = await api.get('/usuarios');
+        setUsers(data);
+        console.log(data);
     }
     getUsers();
    
     }, [])
 
     return (
-        <div>
+        <Container>
             <TopBackground />
-            <h1>Listagem de Usuários</h1>
-            <Button>Verificar Usuário</Button>
 
-        </div>
+            <Title>Usuários Cadastrados</Title>
+
+            <ContainerUser>
+            {users.map( (user) => (
+                <CardUsers key={user.id}>
+                <div>   
+                    <p>Nome: {user.name}</p>
+                    <p>Email: {user.email}</p>
+                    <p>Idade: {user.age}</p>
+                </div>
+                <TrashIcon src={Trash} alt="Lixeira" />
+                </CardUsers>
+            ))}
+            </ContainerUser>
+            <Button type="button" onClick={() => navigate('/')}>Voltar</Button>
+        </Container>
     )
 }
 
---
+++ src/pages/ListUsers/styles.js
@@ -0,0 +1,18 @@
+
+import styled from 'styled-components';
+
+
+export const Title = styled.h2`
+
+`
+export const ContainerUser = styled.div`
+
+`
+export const CardUsers = styled.div`
+
+`
+export const Container = styled.div`
+
+`
+export const TrashIcon = styled.img`
+`
